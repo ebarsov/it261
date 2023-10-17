1c3cdf5f300c8680e6b384a9e99fa53e30dfaef2
ebarsov <109013764+ebarsov@users.noreply.github.com>
2023-10-16 23:07:26 -0700

    Add files via upload

--- website/index.php
+++ website/index.php
@@ -1,103 +1,87 @@
+<?php
+include('./includes/header.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Portal Page</title>
-    <link href="css/styles1.css" type="text/css" rel="stylesheet">
+    <title>Home Page</title>
+<style>
+* {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        header {
+            background-color: ghostwhite;
+        }
+        #hero {
+            background-color: white;
+        }
+
+        body {
+            background-color:white;
+        }
+
+        main {
+            background-color: whitesmoke;
+        }
+
+        aside {
+            background-color:white;
+        }
+
+        h1, h2, h3 {
+            color:burlywood;
+            margin-left: 10px;
+        }
+
+        p {
+            /* margin-bottom: 20px;*/
+            text-align: justify;
+            padding: 15px;
+        }
+
+</style>
 </head>
 <body>
-    <h1><a href= "website/index.php">Elena's IT 261 Portal Page</a></h1>
-    <h2>Welcome</h2>
+
     <div id="wrapper">
-        <nav>
-            <ul>
-                <li><a href=" ">Switch</a></li>
-                <li><a href=" ">Troubleshoot</a></li>
-                <li><a href=" ">Calculator</a></li>
-                <li><a href=" ">Email</a></li>
-                <li><a href=" ">Database</a></li>
-                <li><a href=" ">Gallery</a></li>
-            </ul>
 
+        <div id="hero">
+            <img src="images/washingtonst.png" alt="Welcome">
+        </div>
+        <!-- end hero-->
 
-        </nav>
         <main>
-            <h2>About me</h2>
-            <img class="right" src="images/elena1.png " alt="Elena">
-            <p>I'm a student at Seattle Central College studying web design. I like to plunge into the unknown and learn new things for myself. That's why I'm here.</p>
-            <p> I also like traveling, which is my hobby. My favorite thing to do is visit Europe, especially at Christmas. In this time Europe is most beautiful and elegant! You should check it out… And it does not matter whether you see it: in person or via YouTube. 
-            </p>
-            <!-- <img src="images/elena2.png " alt="Elena "> -->
-
-            <h3>MAMP Screenshot</h3>
-             <img class="screen"  src="images/mamp1.png" alt="MAMP">
-             
-             <h3>Error Screenshot</h3>
-             <img class="error" src="images/error1.png" alt="Error">
-
+            <h1>Welcome to Washington State!</h1>
+            <!--<h2>This is a guide for a 7 day trip at Washington State!</h2> -->
+            <p>Washington is the 18th-largest state, with an area of 71,362 square miles (184,830 km2), and the 13th-most populous state, with more than 7.7 million people. 
+                The majority of Washington's residents live in the Seattle metropolitan area, the center of transportation, business, and industry on Puget Sound, an inlet of the Pacific Ocean consisting of numerous islands, deep fjords and bays carved out by glaciers.
+                 The remainder of the state consists of deep temperate rainforests in the west; mountain ranges in the west, center, northeast, and far southeast; and a semi-arid basin region in the east, center, and south, given over to intensive agriculture. 
+                 Washington is the second most populous state on the West Coast and in the Western United States, after California.</p>
+            <h2>The Mt.Rainier is a Landmark of Washington State</h2>
+            <p> There's nothing like being this close to a volcano. It's one of the many reasons why it's worth it to visit Mt Rainier (see picture up). 
+                Generally, about 2 million people visit Mount Rainier each year. In 2022, the park had 2,371,585 visitors.
+                There is a reason that Washington State license plates feature Mount Rainier. This landmark represents one of the many things that are so wonderful about its home state—
+                with the misty mornings, the fern-filled forests, and the lovely mountain, gracing us with its presence when it pleases, Washington is a place where magic feels real.
+             </p>
         </main>
 
         <aside>
-            <h2>Weekly Class Exercises</h2>
-            <h3>Week 2</h3>
-            <ol>
-                <li><a href="weeks/week2/var.php">var.php</a></li>
-                <li><a href="weeks/week2/vars2.php ">vars2.php</a></li>
-                <li><a href="weeks/week2/currency-logic.php">currency-logic.php</a></li>
-                <li><a href="weeks/week2/currency.php ">currency.php</a></li>
-
-            </ol>
-
-            <h3>Week 3</h3>
-            <ol>
-                <li><a href=" ">something.php</a></li>
-                <li><a href=" ">something.php</a></li>
-                <li><a href=" ">something.php</a></li>
-                <li><a href=" ">something.php</a></li>
-
-            </ol>
-
-            <h3>Week 4</h3>
-            <ol>
-                <li><a href=" ">something.php</a></li>
-                <li><a href=" ">something.php</a></li>
-                <li><a href=" ">something.php</a></li>
-                <li><a href=" ">something.php</a></li>
-
-            </ol>
-
-            <h3>Week 5</h3>
-            <ol>
-                <li><a href=" ">something.php</a></li>
-                <li><a href=" ">something.php</a></li>
-                <li><a href=" ">something.php</a></li>
-                <li><a href=" ">something.php</a></li>
-
-            </ol>
-
-
+            <h3>The Space Needle is a Landmark of Seattle</h3>
+            <img src='./images/needle.png' alt="Needle">
+            <p>The Space Needle is an observation tower in Seattle, Washington, United States.
+                 Considered to be an icon of the city, it has been designated a Seattle landmark. 
+                 Located in the Lower Queen Anne neighborhood, it was built in the Seattle Center for the 1962 World's Fair, which drew over 2.3 million visitors.</p>
         </aside>
-
-</div>
-<!-- close wrapper -->
-
-<footer>
-        <ul>
-            <li>Copyright &copy;
-                2022</li>
-            <li>All Rights Reserved</li>
-            <li><a href="../index.php">Web Design by Elena</a></li>
-            <li><a id="html-checker" href="#">HTML Validation</a></li>
-            <li><a id="css-checker" href="#">CSS Validation</a></li>
-            </ul>
-            
-            <script>
-                    document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
-                    document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
-            </script>
-
-    </footer>
-
+    </div>
+    <!-- end wrapper-->
 </body>
-</html>
+    </html>
+
+    <?php
+    include('./includes/footer.php');
