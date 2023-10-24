8b8e5e2716c0c0ff4fd020bb7efe055bb4423ab2
ebarsov <109013764+ebarsov@users.noreply.github.com>
2023-10-23 22:48:18 -0700

    Add files via upload

---
+++ weeks/week2/week4/test.php
@@ -0,0 +1,13 @@
+<?php
+//ini_set('display_errors', 1);
+
+ini_set ('display_errors', 1);
+ini_set ('display_startup_errors', 1);
+error_reporting (E_ALL);
+//'error_reporting'(E_ALL);
+//display_errors (on);
+$vegetables[]= 'spinach';
+$vegetables[]= 'broccoli';
+$vegetables[]= 'asparagus';
+$vegetables[]= 'kale';
+echo $vegetables;
