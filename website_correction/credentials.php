a8ca6fc61fadc39723479ed4bccb20f8d2318d8f
ebarsov <109013764+ebarsov@users.noreply.github.com>
2023-12-11 20:49:17 -0800

    Delete website_correction/credentials.php

--- website_correction/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php   /// my credentials that will allow me to connect to the database!!!
-
-
-define('DB_NAME', 'elenab5_fall2023');
-define('DB_USER', 'elenab5_fall2023_user');
-define('DB_PASSWORD', '$Lancome2469');
-define('DB_HOST', 'localhost');
